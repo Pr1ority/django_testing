1224448bee06d2fd7494ef4418a9317f5efc9d71
Pr1ority <160697626+Pr1ority@users.noreply.github.com>
2024-05-01 16:16:09 +0300

    Add files via upload

--- ya_news/news/pytest_tests/test_routes.py
+++ ya_news/news/pytest_tests/test_routes.py
@@ -22,10 +22,7 @@ User = get_user_model()
 )
 @pytest.mark.django_db
 def test_pages_availability(client, name, args):
-    if args:
-        url = reverse(name, args=(args.id,))
-    else:
-        url = reverse(name)
+    url = reverse(name, args=(args.id,))
     response = client.get(url)
     assert response.status_code == HTTPStatus.OK
 
