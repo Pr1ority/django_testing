3f0f68857b307119c578e393a3271e18839a9399
Pr1ority <160697626+Pr1ority@users.noreply.github.com>
2024-05-01 15:06:24 +0300

    Add files via upload

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -8,14 +8,14 @@ from news.forms import CommentForm
 
 HOME_URL = reverse('news:home')
 
-
+@pytest.mark.django_db
 def test_news_count(client):
     response = client.get(HOME_URL)
     object_list = response.context['object_list']
     news_count = object_list.count()
     assert news_count == settings.NEWS_COUNT_ON_HOME_PAGE
 
-
+@pytest.mark.django_db
 def test_news_order(client):
     response = client.get(HOME_URL)
     object_list = response.context['object_list']
