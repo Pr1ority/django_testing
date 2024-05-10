655340a4154345a2e8a31e79dfb65636babf4ce0
Pr1ority <160697626+Pr1ority@users.noreply.github.com>
2024-05-10 07:43:15 +0300

    Add files via upload

--- ya_note/notes/tests/test_urls.py
+++ ya_note/notes/tests/test_urls.py
@@ -11,10 +11,10 @@ class TestURLs:
     SIGNUP_URL = reverse('users:signup')
 
     def get_edit_url(self, slug):
-        return reverse('notes:edit', args=[slug])
+        return reverse('notes:edit', kwargs={'slug': slug})
 
     def get_delete_url(self, slug):
-        return reverse('notes:delete', args=[slug])
+        return reverse('notes:delete', kwargs={'slug': slug})
 
     def get_detail_url(self, slug):
         return reverse('notes:detail', args=[slug])
